596587c348c91631b108859c4d3779872de9165f
Siarhei-Chaptsau <s.chaptsau@gmail.com>
2017-10-01 21:37:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function zeros(expression) {
-  // your solution
+  var counter = 0;
+  function factorial(n) {
+    return (n >= 1) ? n * factorial(n - 1) : 1;
+  }  
+
+  var result = factorial(expression);
+
+  if ( result % 10 === 0) {
+    counter++;
+  }
+  alert(counter);
+ }
 }
