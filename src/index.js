4f71e3ad60274b92f9208d214bd54d41299098b9
Siarhei-Chaptsau <s.chaptsau@gmail.com>
2017-10-01 21:44:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 module.exports = function zeros(expression) {
   var counter = 0;
+  var expression;
+  var expressionNumber = parseInt(expression);
   function factorial(n) {
     return (n >= 1) ? n * factorial(n - 1) : 1;
   }  
 
-  var result = factorial(expression);
+  var result = factorial(expressionNumber);
 
   if ( result % 10 === 0) {
     counter++;
